159dfb399560013c161a47dfb44373fc7553086f
RatnikovMax <morfin.ratnikov@mail.ru>
2025-10-30 17:00:38 +0300

    Fix logger import and cleanup functions

--- config.py
+++ config.py
@@ -3,15 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8357063170:AAEpJHkulArjRUWFzMLVFA5yxeDrrX4CpwI")
-ADMIN_ID = os.getenv("1121138765")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден в .env файле")
 
-VK_GROUP_TOKEN = os.getenv("vk1.a.y04ThTBkB2pti_hrrYgTYcVQ1n1XcmVD49GW0YJD6TMJEwxnTdiWC87NWI92g7p1KIaA3FMFulgBeaHxP5Mc3LdckEfD5v3iRg2lHvKD_NcVF-dAa7Bz9CG9TnHH8H6RwQs_bgOTJO0oAuMpuEefjrwqRdXJYAdJTFDWXhTWeX-IPWb6hr8DnRChISxABKK0E3Jb92uiud7x9STKYnHS9w")
-VK_GROUP_ID = os.getenv("232456723")
-VK_ADMIN_ID = os.getenv("370638795")
+# VK переменные
+VK_GROUP_TOKEN = os.getenv("VK_GROUP_TOKEN")
+VK_GROUP_ID = os.getenv("VK_GROUP_ID")
+VK_ADMIN_ID = os.getenv("VK_ADMIN_ID")
 
 if not VK_GROUP_TOKEN:
     print("⚠️  VK_GROUP_TOKEN не найден. VK бот отключен")
